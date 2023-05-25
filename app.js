35ca0fd92b6f42508920e374c616a3625f6e51a6
Sahil Mistry <sahil.m@simformsolutions.com>
2023-05-25 17:44:12 +0530

    feat: create home route * create static files, css file and image directory to load static content

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express  = require('express');
+const homeRouter = require('./routes/home');
 const userRouter = require('./routes/user');
 const adminRouter = require('./routes/admin');
 const blogRouter = require('./routes/blog');
 const app = express();
 
+// static files
+app.use(express.static('public'));
+
 // middlewares
+app.use('/', homeRouter);
+app.use('/blog', blogRouter);
 app.use('/user', userRouter);
 app.use('/admin', adminRouter);
-app.use('/blog', blogRouter);
 
 app.listen(3000, () => console.log('server is listing on port 3000'));
