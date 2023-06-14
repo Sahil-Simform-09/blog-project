12063cc85eef0771e455a2897e4dd4b4571c9a16
Sahil Mistry <sahil.m@simformsolutions.com>
2023-06-14 12:25:04 +0530

    feat: add jwt.verify() in guest.js middleware

--- midlewares/guest.js
+++ midlewares/guest.js
@@ -1,9 +1,27 @@
+const jwt = require('jsonwebtoken');
 const guest = (req, res, next) => {
-    if(!req.cookies.access_token) {
+    const accessToken = req.cookies.access_token;
+    if(!accessToken) {
+        console.log('This accessToken is not sended');
         return next();
-    }
+    } 
 
-    return res.redirect('/');
+    jwt.verify(accessToken, process.env.JWT_TOKEN_SECRET, (err, user) => {
+        if(err) {
+            switch (err.name) {
+                case 'TokenExpiredError':
+                    console.log('accessToken expired');
+                    res.clearCookie('access_token');
+                    next();
+                    break;
+                default:
+                    console.log('invalid accessToken');
+                    next();
+                    break;
+            }
+        }
+        return res.redirect('/');
+    });
+    return ;
 }
-
 module.exports = guest;
